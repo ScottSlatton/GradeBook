af900bf014e9e7b35e79a1f408e14cafd628446c
ScottSlatton <scottslatton.tech@gmail.com>
2019-08-14 22:32:18 -0700

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HelloWorld("Scott");
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("Please pass a name as a parameter.");
+            }
+
+
+        }
+
+        /*  void HelloWorld(string name)
+        {
+            Console.WriteLine($"Hello, {name}");
+        }
+        */
+    }
+}
